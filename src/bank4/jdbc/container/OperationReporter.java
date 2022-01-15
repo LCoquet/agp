7074ced1f14ac87d22b841b2f727cdd602ab2b40
LCoquet <coquet.leo18@gmail.com>
2022-01-15 15:11:30 +0200

    Modified AOP

--- src/bank4/jdbc/container/OperationReporter.java
+++ src/bank4/jdbc/container/OperationReporter.java
@@ -6,23 +6,19 @@ import java.io.IOException;
 
 import org.aspectj.lang.JoinPoint;
 
-import bank4.jdbc.client.AbstractClient;
-
 public class OperationReporter {
 
 	private static final String OPERATION_FILE = "src/bank3/spring/container/Operations.txt";
 	
-	public void treatedOperationEntry(JoinPoint joinPoint) {
+	public void treatedOperationEntry(JoinPoint joinPoint, Object res) {
 		
-		Object[] args = joinPoint.getArgs();
-		AbstractClient client = (AbstractClient) args[0];
 		try {
 			BufferedWriter writer = new BufferedWriter(new FileWriter(OPERATION_FILE, true));
 			writer.write("Dear boss, today, a new operation has been done !");
 			writer.newLine();
 			writer.write("Here is some information about the operation : ");
 			writer.newLine();
-			writer.write(client.getOperation().print(client.getAccount()));
+			writer.write((String) res);
 			writer.newLine();
 			writer.write("-----------------------------------------------------------------------------");
 			writer.newLine();
